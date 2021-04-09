14f69cc733c197b5c5308bc3fcad3cca9bc7002a
Warm4kerT <lalmeida186@gmail.com>
2021-04-09 23:41:23 +0100

    1st commit

---
+++ livraria.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct livro{
+    int ISBM, anoPub, stock;
+    float preco;
+    char *titulo[50], *idioma[25], *primAutor[25], *secAutor[25], *editora[25], *area[25];
+};
+
+struct cliente{
+    
+};
